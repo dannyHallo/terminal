b6f8a2ed42eb90735ce54c28a195c56fe8875b3e
JerryMouWSYM <108749196+JerryMouWSYM@users.noreply.github.com>
2022-07-29 20:09:58 +0800

    setup different satellite

--- Assets/Scripts/Player/LikeASatellite.cs
+++ Assets/Scripts/Player/LikeASatellite.cs
@@ -3,17 +3,81 @@ using UnityEngine;
 
 public class LikeASatellite : MonoBehaviour
 {
+    public enum OrbitWay
+    {
+        // orginal,
+        Yrotation,
+        Zrotation,
+        CustomRotation
+    }
+    public OrbitWay orbitWay;
     public float orbitHeight = 256.0f;
     [Range(0, 1)] public float orbitPos = 0.0f;
     public float orbitSpeed = 0.0001f;
+    public GameObject OrbitCenter;
+    private float Xrotation;
+    private Vector3 cameraPosition;
+    public void Start()
+    {
+        SetUpXRotation();
 
+    }
     private void FixedUpdate()
     {
-        if (orbitPos > 1.0f) orbitPos = 0;
-        if (orbitPos < 0) orbitPos = 1.0f;
-        orbitPos += orbitSpeed * Time.fixedDeltaTime;
+        //if (orbitWay == OrbitWay.orginal)
+        //{
+        //    if (orbitPos > 1.0f) orbitPos = 0;
+        //    if (orbitPos < 0) orbitPos = 1.0f;
+        //    orbitPos += orbitSpeed * Time.fixedDeltaTime;
 
-        transform.position = orbitHeight * new Vector3(Mathf.Sin(orbitPos * 2 * Mathf.PI), 0, Mathf.Cos(orbitPos * 2 * Mathf.PI));
-        transform.LookAt(new Vector3(0, 0, 0), Vector3.up);
+        //    transform.position = orbitHeight * new Vector3(Mathf.Sin(orbitPos * 2 * Mathf.PI), 0, Mathf.Cos(orbitPos * 2 * Mathf.PI));
+        //    transform.LookAt(new Vector3(0, 0, 0), Vector3.up);
+        //}
+
+        SetUpXRotation();
+        //CameraSetUp();
+
+        if (orbitWay == OrbitWay.Yrotation)
+        {
+            Transform centerRotation = OrbitCenter.transform;
+            Vector3 centerRotationVector = OrbitCenter.transform.eulerAngles;
+            centerRotationVector.y += orbitSpeed;
+            centerRotation.eulerAngles = centerRotationVector;
+
+        }
+        if (orbitWay == OrbitWay.Zrotation)
+        {
+            Transform centerRotation = OrbitCenter.transform;
+            Vector3 centerRotationVector = OrbitCenter.transform.eulerAngles;
+            centerRotationVector.z += orbitSpeed;
+            centerRotation.eulerAngles = centerRotationVector;
+
+        }
+        if (orbitWay == OrbitWay.CustomRotation)
+        {
+
+
+        }
+
+
+        CameraSetUp();
+    }
+
+    public void SetUpXRotation()
+    {
+        Transform centerRotation = OrbitCenter.transform;
+        Vector3 centerRotationVector = OrbitCenter.transform.eulerAngles;
+        centerRotationVector.x = Xrotation;
+        centerRotation.eulerAngles = centerRotationVector;
+        Xrotation = centerRotationVector.x;
+    }
+    /// <summary>
+    /// Setupheight
+    /// </summary>
+    public void CameraSetUp()
+    {
+
+        cameraPosition = new Vector3 (0,0,orbitHeight);
+        transform.localPosition = cameraPosition;
     }
 }
