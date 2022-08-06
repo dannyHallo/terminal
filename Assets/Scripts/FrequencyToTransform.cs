6a65ef2938150623c262ec974154d4cee56692ca
JerryMouWSYM <108749196+JerryMouWSYM@users.noreply.github.com>
2022-08-06 11:51:30 +0800

    added SoundToTransform

---
+++ Assets/Scripts/FrequencyToTransform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrequencyToTransform : MonoBehaviour
+{
+    public Transform controlledObject;
+    public AudioProcessor audioProcessor;
+    // Start is called before the first frame update
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
