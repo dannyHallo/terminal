fd1a6cd87aaf1a5674717c87481b65e66252d714
JerryMouWSYM <108749196+JerryMouWSYM@users.noreply.github.com>
2022-08-06 14:36:56 +0800

    pickup UI

--- Assets/Scripts/Pickups/Pickable.cs
+++ Assets/Scripts/Pickups/Pickable.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [RequireComponent(typeof(Collider))]
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(AudioClip))]
 public class Pickable : MonoBehaviour
 {
+    public GameObject PressE;
     private void Start()
     {
-
+        PressE= GameObject.Find("Press E To Pickup"); 
     }
 
     private void Update()
@@ -20,11 +22,35 @@ public class Pickable : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag != "Player")
+            PressE.SetActive(true);
             return;
-        GameObject player = GameObject.Find("Player");
-        PlayerController.InstrumentTypes instrumentType = PlayerController.InstrumentTypes.Guitar;
 
-        player.GetComponent<PlayerController>().UseInstrument(instrumentType);
-        Destroy(gameObject);
+
     }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag != "Player")
+        {
+            PressE.SetActive(false);
+            return;
+        }
+            
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            {
+            GameObject player = GameObject.Find("Player");
+            PlayerController.InstrumentTypes instrumentType = PlayerController.InstrumentTypes.Guitar;
+
+            player.GetComponent<PlayerController>().UseInstrument(instrumentType);
+            Destroy(gameObject);
+            PressE.SetActive(false);
+        }
+
+    }
+
+
+
 }
+
---
+++ Assets/UIManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject pickUpUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
