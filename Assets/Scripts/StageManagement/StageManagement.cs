c5b27f57e812aceb9c9a8fb7ac067dfe07235b62
JerryMouWSYM <108749196+JerryMouWSYM@users.noreply.github.com>
2022-08-07 21:26:57 +0800

    model input and stageManagement

---
+++ Assets/Scripts/StageManagement/StageManagement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class StageManagement : MonoBehaviour
+{
+    public CinemachineVirtualCamera followingCam;
+    public CinemachineVirtualCamera orbitCam;
+    public GameObject WorldEdge;
+    public GameObject orginalPlane;
+
+
+
+    public void StageSwitch(int SwitchCount)
+    {
+        if (SwitchCount == 1)
+        {
+
+        }
+        if (SwitchCount == 2)
+        {
+
+        }
+        if (SwitchCount == 3)
+        {
+
+        }
+        if (SwitchCount == 4)
+        {
+
+        }
+        if (SwitchCount == 5)
+        {
+
+        }
+        if (SwitchCount == 6)
+        {
+
+        }
+        if (SwitchCount == 7)
+        {
+
+        }
+        if (SwitchCount == 8)
+        {
+
+        }
+        ;
+    }
+}
